4b7c0476ea8e3b64867fee9976dcc0ffc8310aeb
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-05-17 18:26:18 +0200

    FIX

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 //inseriemtno dati utente
-let km = prompt ('Inserisci i km da percorrere');
+let km = parseInt(prompt('Inserisci i km da percorrere'));
 
-let età = prompt ('Inserisci la tua età');
+let età = parseInt(prompt('Inserisci la tua età'));
 
 //costo fisso del viaggio
-const costo = '0.21';
+const costo = 0.21;
+
+let prezzo_completo = (km * costo);
 
 //alert nel caso in cui non si inseriscano valori numerici
 if (isNaN(km) || isNaN(età)){
     alert('Prego, inserire dei valori validi!');
 }
-
-//calcolo viaggio
-let prezzo_completo = (km * costo);
+else{
+    //calcolo viaggio
 
 if (età < 18){
     under_18 = (prezzo_completo - (prezzo_completo * 20 / 100)).toFixed(2)
@@ -22,6 +23,8 @@ if (età < 18){
     document.getElementById('prezzo_biglietto').innerHTML = `Il tuo totale è: ${over_65}$`;
 }else {
     document.getElementById('prezzo_biglietto').innerHTML = `Il tuo totale è: ${prezzo_completo}$`;
+}
+
 }
 
 
